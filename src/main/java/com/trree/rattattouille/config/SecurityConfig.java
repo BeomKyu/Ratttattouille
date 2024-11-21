73a6850d16e5cb84d743b5511b26e60aef1243e4
beomkyu choi <beomkyuchoi069@gmail.com>
2024-11-21 15:23:45 +0900

    Sample -> Rattattouille 수정 nonce 토큰 검증 과정이 profileToken 리턴하도록 수정

--- src/main/java/com/trree/rattattouille/config/SecurityConfig.java
+++ src/main/java/com/trree/rattattouille/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
                 .requestMatchers("/nonce-tokens/**").hasRole("PROFILE")
                 .anyRequest().denyAll()
